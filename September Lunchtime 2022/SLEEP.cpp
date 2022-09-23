5f387793b4fcb2104d8f8a666afc4038012fc043
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-09-23 23:52:10 +0530

    Create SLEEP.cpp

---
+++ September Lunchtime 2022/SLEEP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(x<7)
+	        cout<<"YES"<<endl;
+	    
+	    else
+	        cout<<"NO"<<endl;
+	}
+	return 0;
+}
