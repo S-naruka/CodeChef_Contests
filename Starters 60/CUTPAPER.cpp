a6eb9224a1fd895e3039e6314d3c30ff4fd9cda1
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-10-12 23:01:18 +0530

    Create CUTPAPER.cpp

---
+++ Starters 60/CUTPAPER.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{ int n,k,x;
+	    cin>>n>>k;
+	    
+	  x=n/k;  
+	  
+	  cout<<x*x<<endl; 
+	   
+	}
+	return 0;
+}
