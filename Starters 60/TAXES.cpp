159032cfae7decb534e1e27493e6d4a7118dcfad
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2022-10-12 23:00:20 +0530

    Create TAXES.cpp

---
+++ Starters 60/TAXES.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{ int x;
+	    cin>>x;
+	    
+	  if(x>100)
+	    cout<<x-10<<endl;
+	    
+	  else
+	    cout<<x<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
