bffe5f9d480c5fd8e7f77bdb28ccced5af44e4fe
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-01-11 23:02:58 +0530

    Create TABLETS.cpp

---
+++ Starters 73/TABLETS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{ 
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    if(x<=(y/3))
+	        cout<<"YES"<<endl;
+	        
+	    else
+	        cout<<"NO"<<endl;
+	   
+	   
+	}
+	return 0;
+}
