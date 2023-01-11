8e6242c806b7a662b6ca8513a2f819e7dcc79db7
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-01-11 23:02:31 +0530

    Create LUDO.cpp

---
+++ Starters 73/LUDO.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{ 
+	    int x;
+        cin>>x;
+	    
+	    if(x==6)
+	        cout<<"YES"<<endl;
+	    
+	    else
+	        cout<<"NO"<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
