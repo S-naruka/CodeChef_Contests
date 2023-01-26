d7f7d89696ace22ae36823933b6674b68740d9fe
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-01-27 00:02:44 +0530

    Create TAXSAVING.cpp

---
+++ Starters 75/TAXSAVING.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    cout<<x-y<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
