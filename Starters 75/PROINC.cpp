77afa0da0fedb88a44962fb87d2644c7416eec5a
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-01-27 00:03:16 +0530

    Create PROINC.cpp

---
+++ Starters 75/PROINC.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    y=x-y;
+	    x=1.1*x;
+	    cout<<x-y<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
