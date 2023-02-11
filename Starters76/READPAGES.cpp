46eb7c7ffb6f4e4422443507adf8fe382b02bd55
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-02-11 19:01:01 +0530

    Create READPAGES.cpp

---
+++ Starters76/READPAGES.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x,y;
+	    cin>>n>>x>>y;
+	    if(y*x>=n)
+	        cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
