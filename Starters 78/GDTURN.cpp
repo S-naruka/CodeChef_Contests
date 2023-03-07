4e386f4630533725a76004d9185384b7e45fd386
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-03-07 20:58:58 +0530

    Create  GDTURN.cpp

---
+++ Starters 78/GDTURN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x+y>6)
+	        cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
