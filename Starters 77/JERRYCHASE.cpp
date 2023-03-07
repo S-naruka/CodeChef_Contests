63f0dc8553ac8446f74b3244c5c3a9f010022793
Dhananjay Singh <112682377+S-naruka@users.noreply.github.com>
2023-03-07 20:56:21 +0530

    Create JERRYCHASE.cpp

---
+++ Starters 77/JERRYCHASE.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(y>x)
+	        cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+	    
+	   
+	   
+	}
+	return 0;
+}
